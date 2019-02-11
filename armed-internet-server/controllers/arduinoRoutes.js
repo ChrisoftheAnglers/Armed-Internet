6a5a9f54535c4f3d5069a319ca597c2713ef4a4f
Christian <chansen302@gmail.com>
2019-02-11 10:46:00 -0800

    Forgot to export routes (and fixed).  Also, changed the delete route to drop the whole collection, which should be a workable solution so long as I get React JS to drop the collection at session end.

--- armed-internet-server/controllers/arduinoRoutes.js
+++ armed-internet-server/controllers/arduinoRoutes.js
@@ -20,16 +20,18 @@ router.post('/', function(req, res) {
 // Path to update command information
 // Should be used almost exclusively once the the session has been initialized
 // Should only have one id since only one document should persist (may replace with findOneAndReplace)
-router.put('/:id', function(req, res) {
-    Arduino.findByIdAndUpdate(req.params.id, req.body)
+router.put('/', function(req, res) {
+    Arduino.findOneAndUpdate({}, req.body)
     .then(results => res.json(results))
     .catch(error => res.status(500).json(error));
 });
 
 // Usage may vary: this could very well be limited to debugging, but may end up being used at session end so
 // a new set can replace it
-router.delete('/:id', function(req, res) {
-    Arduino.findByIdAndDelete(req.params.id)
+router.delete('/', function(req, res) {
+    Arduino.collection.drop()
     .then(results => res.json(results))
     .catch(error => res.status(500).json(error));
 });
+
+module.exports = router;
