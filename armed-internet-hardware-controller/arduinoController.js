1264129740ccfd7c064ce4d2284f76a1e4406ffd
Christian <chansen302@gmail.com>
2019-02-14 00:11:43 -0800

    Mostly finished with the hardware controller, just needs testing

--- armed-internet-hardware-controller/arduinoController.js
+++ armed-internet-hardware-controller/arduinoController.js
@@ -1,18 +1,14 @@
-/* // Initialize Firebase
+// Initialize Firebase
 var firebase = require('firebase');
 
 firebase.initializeApp({
-    apiKey: "AIzaSyDQefq8pw-Exyooa_N2wh27fzA3v1LV1IE",
-    authDomain: "arduino-control-testing.firebaseapp.com",
-    databaseURL: "https://arduino-control-testing.firebaseio.com",
-    projectId: "arduino-control-testing",
-    storageBucket: "arduino-control-testing.appspot.com",
-    messagingSenderId: "493184510884"
+    apiKey: "AIzaSyCz8q6ms8i0LufyDzireMfCGUQKOcQmzQ4",
+    authDomain: "arduinocommand-d9fe5.firebaseapp.com",
+    databaseURL: "https://arduinocommand-d9fe5.firebaseio.com",
+    projectId: "arduinocommand-d9fe5",
+    storageBucket: "arduinocommand-d9fe5.appspot.com",
+    messagingSenderId: "780218622646"
 });
-
-firebase.auth().signInAnonymously().catch(function(error) {
-    console.log("Error Code: " + error.code);
-}) */
 
 var five = require('johnny-five'); //includes module indicated by string
 var board = new five.Board(); //Instantiate object class 'board' with parent 'five'
@@ -22,7 +18,33 @@ board.on('ready', function() {
     this.pinMode(12, 1);
     this.digitalWrite(12, 1);
     // Initialization of Servos with starting positions
-    var gripper = new five.Servo({
+    let Braccio = {
+            gripper: new five.Servo({
+            pin: 3,
+            startAt: 73
+            }),
+            wrist_rot: new five.Servo({
+                pin: 6,
+                startAt: 0
+            }),
+            wrist_ver: new five.Servo({
+                pin: 5,
+                startAt: 170
+            }),
+            elbow: new five.Servo({
+                pin: 9,
+                startAt: 180
+            }),
+            shoulder: new five.Servo({
+                pin: 10,
+                startAt: 40
+            }),
+            base: new five.Servo({
+                pin: 11,
+                startAt: 0
+            })
+        }
+    /* var gripper = new five.Servo({
         pin: 3,
         startAt: 73
     });
@@ -45,38 +67,65 @@ board.on('ready', function() {
     var base = new five.Servo({
         pin: 11,
         startAt: 0
-    });
+    }); */
 
     // TODO: event function to get info from database and use it to change servo position as necessary
-    
+    function servoMovement(update) {
+        Braccio.base.to(update.servos.base);
+        Braccio.elbow.to(update.servos.elbow);
+        Braccio.gripper.to(update.servos.gripper);
+        Braccio.shoulder.to(update.servos.shoulder);
+        Braccio.wrist_rot.to(update.servos.wristRot);
+        Braccio.wrist_ver.to(update.servos.wristVert);
+    }
 
-    /* base.center();
-    shoulder.center();
-    elbow.center();
-    wrist_rot.center();
-    wrist_ver.center();
-    gripper.center(); */
+    // Initialize LED variables
+    let led = new five.Led(4); //Pin 13
+    let power = false;
+    let blink = false;
 
-    /* var led = new five.Led(13); //Pin 13
-    led.blink(500); //500 ms interval
-    on = true; //Set on to true
-    firebase.database().ref().set({
-        on: on
-    })
-    //This is nested inside board.on because led objects (and others) WILL NOT INITIALIZE PROPERLY UNTIL board.on
-    //'ready' occurs (probably to prevent signals from being sent to the Arduino board before it is can accept them)
-    firebase.database().ref().on("value", function(update) {
-        console.log(update);
-        on = update.val().on;
-        if (on) {
+    // Function for controlling LED called by Firebase changes
+    function ledSwitch(update) {
+        power = update.val().ledPower;
+        blink = update.val().ledBlink;
+        if (power && blink) {
             led.blink(500); //500 ms interval
+        }
+        else if (power && !blink) {
+            led.stop(); // To make sure that the blinking stops but 
+            led.on();
         }
         else {
             led.stop().off(); //Will turn off the blinking LED (note that both off() and stop() are required)
         }
+    }
+
+    // Initialize Piezo Speaker, Songs, and States
+    let piezo = new five.Piezo(2);
+
+    // Function to get request from Firebase and play Piezo tune
+    function playtune(update) {
+        if (update.playTrack1 === true) {
+            firebase.database().ref().update({
+                playTrack1: false
+            })
+        }
+        else if (update.playTrack2 === true) {
+            firebase.database().ref().update({
+                playTrack2: false
+            })
+        }
+    }
+
+    // Event function for Firebase update on value which will return the object containing the necessary information
+    firebase.database().ref().on("value", function(update) {
+        console.log(update);
+        ledSwitch(update);
+        servoMovement(update);
+        playtune(update);
     }, function(error) {
         console.log("Error Code: " + error.code);
-    }) */
+    })
 })
 
 /* firebase.database().ref().on("value", function(update) {
